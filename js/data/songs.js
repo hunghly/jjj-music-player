9878af8021f2c4847b164385ef6ffdb4b65f8deb
Hung Ly <hunghly@gmail.com>
2020-01-01 12:37:53 -0600

    adjusted music data

--- js/data/songs.js
+++ js/data/songs.js
@@ -1,56 +1,56 @@
 export const songsList = [{
     "id": "1",
     "url": "../../music/nice-for-what.mp3",
-    "time": "03:30",
+    "time": "03:31",
     "title": "Nice For What",
     "artist": "Drake"
   },
   {
     "id": "2",
     "url": "../../music/gods-plan.mp3",
-    "time": "03:18",
+    "time": "03:19",
     "title": "God's Plan",
     "artist": "Drake"
   },
   {
     "id": "3",
     "url": "../../music/professional-rapper.mp3",
-    "time": "05:52",
+    "time": "05:54",
     "title": "Professional Rapper ft. Snoop Dog",
     "artist": "Lil Dicky"
   },
   {
     "id": "4",
     "url": "../../music/yellow.mp3",
-    "time": "04:29",
+    "time": "04:33",
     "title": "Yellow",
     "artist": "Coldplay"
   },
   {
     "id": "5",
     "url": "../../music/get-lucky.mp3",
-    "time": "06:09",
+    "time": "06:08",
     "title": "Get Lucky",
     "artist": "Daft Punk"
   },
   {
     "id": "6",
     "url": "../../music/shape-of-my-heart.mp3",
-    "time": "04:43",
+    "time": "04:38",
     "title": "Shape Of My Heart",
     "artist": "Sting"
   },
   {
     "id": "7",
     "url": "../../music/seven-days.mp3",
-    "time": "03:57",
+    "time": "05:28",
     "title": "7 days",
     "artist": "craig David"
   },
   {
     "id": "8",
     "url": "../../music/dilemma.mp3",
-    "time": "04:42",
+    "time": "04:43",
     "title": "Nelly Dilemma ft. keylly Rowland",
     "artist": "Nelly"
   },
@@ -64,21 +64,21 @@ export const songsList = [{
   {
     "id": "10",
     "url": "../../music/congratulations.mp3",
-    "time": "03:40",
+    "time": "03:47",
     "title": "Congratulations ft. Quavo",
     "artist": "Post Malone"
   },
   {
     "id": "11",
     "url": "../../music/rockstar.mp3",
-    "time": "04:01",
+    "time": "04:02",
     "title": "Rockstar",
     "artist": "Post Malone"
   },
   {
     "id": "12",
     "url": "../../music/sad.mp3",
-    "time": "02:46",
+    "time": "02:47",
     "title": "Sad",
     "artist": "XXXtentacion"
   },
